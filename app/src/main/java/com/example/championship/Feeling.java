4a109ff722882e48195e57001b3fcc4dc69c1c56
zlobinovr@gmail.com <zlobinovr@gmail.com>
2023-03-21 02:36:54 +0300

    First commit

---
+++ app/src/main/java/com/example/championship/Feeling.java
@@ -0,0 +1,7 @@
+package com.example.championship;
+
+public class Feeling {
+    public String title;
+    public int position;
+    public String image;
+}
---
+++ app/src/main/java/com/example/championship/Photo.java
@@ -0,0 +1,8 @@
+package com.example.championship;
+
+import android.graphics.Bitmap;
+
+public class Photo {
+    public Bitmap image;
+    public String timestamp;
+}
